f6d0f7602df38068efc24c1d59f10e3667468ca0
vvkochmi <62569109+vvkochmi@users.noreply.github.com>
2025-03-27 01:08:43 +0300

    Add files via upload

---
+++ MalenomAPI/MalenomAPI/Models/Image.cs
@@ -0,0 +1,9 @@
+﻿namespace MalenomAPI.Models
+{
+    public class Image
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public byte[] Img { get; set; }
+    }
+}
